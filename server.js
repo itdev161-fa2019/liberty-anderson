4f1458f0fad6fcc95fb45729f17977c54feaac92
Lee Roz <anderl85@gmatc.matc.edu>
2019-11-19 23:28:54 -0600

    added delete post endpoint

--- server.js
+++ server.js
@@ -234,3 +234,30 @@ app.get('/api/posts/:id', auth, async (req, res) => {
 		res.status(500).send('Server error');
 	}
 });
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete('/api/posts/:id', auth, async (req, res) => {
+	try{
+		const post = await Post.findById(req.params.id);
+
+		//make sure post was found
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		//make sure requesting user made post
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'User not authorized' });
+		}
+
+		await post.remove();
+
+		res.json({ msg: 'Post removed' });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Server error');
+	}
+});
